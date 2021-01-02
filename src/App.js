19545cf8049d745c230a60698864efff82c09202
Ben Thompson <btomo86@gmail.com>
2021-01-02 14:50:41 +0000

    mapped over upcoming games and new games and added global styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { loadGames } from "./actions/gamesActions";
 import Home from "./pages/Home";
+import GlobalStyles from "./components/GlobalStyles";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Home />
     </div>
   );
